1edb085e1b88ac189fcfa6f739f65de1fcdc1322
Manish Sharma <81001090+manishsharma864@users.noreply.github.com>
2022-07-28 11:18:54 +0530

    change in next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  typescript:{
+    ignoreBuildErrors: true,
+  },
   swcMinify: true,
   images:{
     domains:['external-content.duckduckgo.com','lh3.googleusercontent.com']
